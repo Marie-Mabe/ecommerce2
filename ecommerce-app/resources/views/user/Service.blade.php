0386f9a20aaa8069565024600e995097a86ab332
Mabé <topmabe@gmail.com>
2025-05-16 02:51:22 +0000

    Affichage des produits sur le dashboard de l'utilisateur

--- ecommerce-app/resources/views/user/Service.blade.php
+++ ecommerce-app/resources/views/user/Service.blade.php
@@ -1,64 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="author" content="Untree.co">
-  <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
+@extends('user.navigation.layout')
 
-  <meta name="description" content="" />
-  <meta name="keywords" content="bootstrap, bootstrap4" />
+@section('tittle', 'Service')
 
-  <!-- Bootstrap CSS -->
-  <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-  <link href="{{ asset('css/tiny-slider.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-  <title>Furni Free Bootstrap 5 Template for Furniture and Interior Design Websites by Untree.co</title>
-</head>
-
-<body>
-  <!-- Start Header/Navigation -->
-			<!-- Start Header/Navigation -->
-<nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
-    <div class="container">
-        <a class="navbar-brand" href="{{ route('user.dashboard') }}">Furni<span>.</span></a>
-
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarsFurni">
-            <ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
-                <li class="nav-item {{ request()->routeIs('user.dashboard') ? 'active' : '' }}">
-                    <a class="nav-link" href="{{ route('user.dashboard') }}">Home</a>
-                </li>
-                <li class="{{ request()->routeIs('user.shop') ? 'active' : '' }}">
-                    <a class="nav-link" href="{{ route('user.shop') }}">Shop</a>
-                </li>
-                <li class="{{ request()->routeIs('user.about') ? 'active' : '' }}">
-                    <a class="nav-link" href="{{ route('user.about') }}">About us</a>
-                </li>
-                <li class="{{ request()->routeIs('user.service') ? 'active' : '' }}">
-                    <a class="nav-link" href="{{ route('user.service') }}">Services</a>
-                </li>
-                <li class="{{ request()->routeIs('user.blog') ? 'active' : '' }}">
-                    <a class="nav-link" href="{{ route('user.blog') }}">Blog</a>
-                </li>
-                <li class="{{ request()->routeIs('user.contact') ? 'active' : '' }}">
-                    <a class="nav-link" href="{{ route('user.contact') }}">Contact us</a>
-                </li>
-            </ul>
-
-            <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
-                <li><a class="nav-link" href="#"><img src="{{ asset('images/user.svg') }}"></a></li>
-                <li><a class="nav-link" href="{{ route('user.cart') }}"><img src="{{ asset('images/cart.svg') }}"></a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
-<!-- End Header/Navigation -->
-  <!-- End Header/Navigation -->
+@section('content')
 
   <!-- Start Hero Section -->
   <div class="hero">
@@ -140,8 +84,8 @@
           <h2 class="mb-4 section-title">Crafted with excellent material.</h2>
           <p class="mb-4">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique.</p>
           <p><a href="#" class="btn">Explore</a></p>
-        </div> 
-        
+        </div>
+
         <!-- Start Column 2 -->
         <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
           <a class="product-item" href="#">
@@ -153,7 +97,7 @@
             </span>
           </a>
         </div>
-        
+
         <!-- Start Column 3 -->
         <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
           <a class="product-item" href="#">
@@ -165,7 +109,7 @@
             </span>
           </a>
         </div>
-        
+
         <!-- Start Column 4 -->
         <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
           <a class="product-item" href="#">
@@ -226,76 +170,12 @@
     </div>
   </div>
   <!-- End Testimonial Slider -->
+  @endsection
 
-  <!-- Start Footer Section -->
-  <footer class="footer-section">
-    <div class="container relative">
-      <div class="sofa-img">
-        <img src="{{ asset('images/sofa.png') }}" alt="Image" class="img-fluid">
-      </div>
-
-      <div class="row">
-        <div class="col-lg-8">
-          <div class="subscription-form">
-            <h3 class="d-flex align-items-center">
-              <span class="me-1"><img src="{{ asset('images/envelope-outline.svg') }}" alt="Image" class="img-fluid"></span>
-              <span>Subscribe to Newsletter</span>
-            </h3>
-            <form action="#" class="row g-3">
-              <div class="col-auto">
-                <input type="text" class="form-control" placeholder="Enter your name">
-              </div>
-              <div class="col-auto">
-                <input type="email" class="form-control" placeholder="Enter your email">
-              </div>
-              <div class="col-auto">
-                <button class="btn btn-primary">
-                  <span class="fa fa-paper-plane"></span>
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-
-      <div class="row g-5 mb-5">
-        <div class="col-lg-4">
-          <div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Furni<span>.</span></a></div>
-          <p class="mb-4">Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque habitant</p>
-          <ul class="list-unstyled custom-social">
-            <li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
-            <li><a href="#"><span class="fa fa-brands fa-twitter"></span></a></li>
-            <li><a href="#"><span class="fa fa-brands fa-instagram"></span></a></li>
-            <li><a href="#"><span class="fa fa-brands fa-linkedin"></span></a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-8">
-          <div class="row links-wrap">
-            <!-- Footer links columns -->
-          </div>
-        </div>
-      </div>
-
-      <div class="border-top copyright">
-        <div class="row pt-4">
-          <div class="col-lg-6">
-            <p class="mb-2 text-center text-lg-start">Copyright &copy;<script>document.write(new Date().getFullYear());</script>. All Rights Reserved. &mdash; Designed with love by <a href="https://untree.co">Untree.co</a> Distributed By <a href="https://themewagon.com">ThemeWagon</a></p>
-          </div>
-          <div class="col-lg-6 text-center text-lg-end">
-            <ul class="list-unstyled d-inline-flex ms-auto">
-              <li class="me-4"><a href="#">Terms &amp; Conditions</a></li>
-              <li><a href="#">Privacy Policy</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
-  <!-- End Footer Section -->
+@section('scripts')
 
   <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
   <script src="{{ asset('js/tiny-slider.js') }}"></script>
   <script src="{{ asset('js/custom.js') }}"></script>
-</body>
-</html>
+
+@endsection
